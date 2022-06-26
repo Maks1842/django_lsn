309f5211d24eb7d7e46afc01570db8e855573b76
Maks1842 <maks1842@yandex.ru>
2022-06-26 16:36:50 +0300

    Старт. Учебный проект "Тренажер английского"

---
+++ english_trainer/trainer/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Words(models.Model):
+    word_eng = models.CharField(max_length=150, verbose_name='Английский')
+    word_transcrip = models.CharField(max_length=150, verbose_name='Транскрипция', blank=True)
+    word_rus = models.CharField(max_length=150, verbose_name='Русский')
+    comment = models.TextField(verbose_name='Комментарий', blank=True)
+    audio = models.FileField(upload_to='audio/%Y/%m/%d/', verbose_name='Аудио', blank=True)    # blank=True  - значит поле не обязательное
+    date_eng_lessons = models.DateField(auto_now=True, verbose_name='Дата изучения английского слова')
+    date_rus_lessons = models.DateField(auto_now=True, verbose_name='Дата изучения русского слова')
+
+    def __str__(self):
+        return self.word_eng
+
+
+class Statistics(models.Model):
+    date_lessons = models.DateField(auto_now=True, verbose_name='Дата изучения слов')
+    sum_word_eng = models.IntegerField(verbose_name='Количество изученных английских слов')
+    sum_word_rus = models.IntegerField(verbose_name='Количество изученных русских слов')
+
+
+class SettingsTrainer(models.Model):
+    count_words = models.IntegerField(verbose_name='Изучить слов')
+    min_days = models.IntegerField(verbose_name='Минимальное количество дней между тренировками')
+    max_days = models.IntegerField(verbose_name='Максимальное количество дней между тренировками')
