67a62e681982cd228732c4a4d127cb0e4a0c876e
Maks1842 <maks1842@yandex.ru>
2022-09-07 11:03:34 +0300

    Vue.js --> cryptonomicon - 3. add tailwing.css

--- english_trainer/frontend/cryptonomicon/src/main.js
+++ english_trainer/frontend/cryptonomicon/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "./assets/tailwind.css"         // Данная строка добавляется автоматически при установке tailwind
 
 createApp(App).mount('#app')
